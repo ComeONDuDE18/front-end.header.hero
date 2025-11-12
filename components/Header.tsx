6ecb660a3da57d63eb606cfba46cfff66a45b91a
ComeONDuDE18 <vibilous6@gmail.com>
2025-11-12 13:44:06 +0100

    go

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,24 +2,28 @@ import css from './Header.module.css';
 import Link from 'next/link';
 import Image from 'next/image';
 
-
 const Header = () => {
-    return (
-<header className={css.header}>
-      <Link href="/" aria-label="Home" className={css.logoLink}>
-        <Image
-          src="/img/svg/logo.svg"   
-          height={40}
-          alt="Подорожники"
-        />
-        <span className={css.logoText}>Подорожники</span>
-      </Link>
+  return (
+    <header className={css.header}>
+      <div>
+        <Link href="/">
+          <Image
+            src="/logo.png"
+            alt="Подорожники"
+            width={120}
+            height={40}
+          />
+          <span className={css.title}>Подорожники</span>
+        </Link>
+      </div>
+
       <nav aria-label="Main Navigation">
         <ul className={css.navigation}>
-          <Link href="/">Головна</Link>
-          <a href="#stories">Історії</a>
-          <a href="#travelers">Мандрівники</a>
-
+          <li><Link href="/" className={css.navLink}>Головна</Link></li>
+          <li><a href="#stories" className={css.navLink}>Історії</a></li>
+          <li><a href="#travelers" className={css.navLink}>Мандрівники</a></li>
+          <li><Link href="/sign-up" className={`${css.navLink} ${css.signUpBtn}`}>Реєстрація</Link></li>
+          <li><Link href="/sign-in" className={`${css.navLink} ${css.signInBtn}`}>Вхід</Link></li>
         </ul>
       </nav>
     </header>
