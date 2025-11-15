0173837ce5bb3b8af04e9419f1cf9cd835bba20a
ComeONDuDE18 <vibilous6@gmail.com>
2025-11-15 23:59:56 +0100

    desktopVersion

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,4 @@
-import css from './Header.module.css';
+import css from '../app/css/Header.module.css';
 import Link from 'next/link';
 
 
@@ -7,11 +7,11 @@ const Header = () => {
     <header className={css.header}>
       <div className={css.logo}>
         <Link href="/">
-          <svg width="28" height="28" aria-hidden="true">
+          <svg width="23" height="23" aria-hidden="true">
             <use href="/icon.svg#icon-plant" />
           </svg>
-          <span className={css.title}>Подорожники</span>
         </Link>
+        <span className={css.title}>Подорожники</span>
       </div>
 
       <nav aria-label="Main Navigation">
@@ -19,8 +19,8 @@ const Header = () => {
           <li><Link href="/" className={css.navLink}>Головна</Link></li>
           <li><a href="#stories" className={css.navLink}>Історії</a></li>
           <li><a href="#travelers" className={css.navLink}>Мандрівники</a></li>
-          <li><Link href="/sign-up" className={`${css.navLink} ${css.signUpBtn}`}>Реєстрація</Link></li>
-          <li><Link href="/sign-in" className={`${css.navLink} ${css.signInBtn}`}>Вхід</Link></li>
+          <li><Link href="/sign-up" className={css.signInBtn}>Вхід</Link></li>
+          <li><Link href="/sign-in" className={css.signUpBtn}>Реєстрація</Link></li>
         </ul>
       </nav>
     </header>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,11 +1,13 @@
-import css from './Hero.module.css';
+import css from '../app/css/Hero.module.css';
 
 const Hero = () => {
   return (
     <section className={css.hero}>
         <div className={css.content}>
-            <h1 className={css.title}>Відкрийте світ подорожей з нами!</h1>
-            <p className={css.subtittle}>
+            <h1 className={css.title}>
+                Відкривайте світ <br /> подорожей з нами!
+            </h1>
+            <p className={css.subtitle}>
                 Приєднуйтесь до нашої спільноти мандрівників, де ви зможете ділитися своїми історіями та отримувати натхнення для нових пригод. Відкрийте для себе нові місця та знайдіть однодумців!</p>
             </div>
             <a href="#aboutus" className={css.moreButton}>Докладніше</a>
